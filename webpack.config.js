cb41acfd720eaf72c2a402e0649909bcb42aab64
Andres Jurado <anmijurane@gmail.com>
2020-09-14 17:35:23 -0500

    entorno listo

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = requere('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.export = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js'
+  },
+  resolve: {
+    extensions: ['.js']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin([
+      {
+        inject: true,
+        template: './public/index.html',
+        filename: './index.html',
+      }
+    ])
+  ]
+}
