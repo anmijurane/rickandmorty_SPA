0d6afd0cf3fbb142a92cb479e17ee7474577407a
Andres Jurado <anmijurane@gmail.com>
2020-09-15 19:00:33 -0500

    correcciones en la resolucion de rutas

--- src/utils/resolveRoutes.js
+++ src/utils/resolveRoutes.js
@@ -1,6 +1,6 @@
 const resolveRoutes = (route) => {
   if (route.length <= 3) {
-    let validRoute = route === '/rickandmorty_SPA/' ? route : '/:id';
+    let validRoute = route === '/' ? route : '/:id';
     return validRoute;
   }
   return `/${route}`;
